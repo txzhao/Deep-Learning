2ddaafe9469e058e51841b798e01978e69459a72
Tianxiao Zhao <tzh@kth.se>
2017-05-05 12:59:55 +0200

    Update init_para.m

--- assign3/code/basic/init_para.m
+++ assign3/code/basic/init_para.m
@@ -1,9 +1,11 @@
-function [W, b] = init_para(m, ~)
+function [W, b] = init_para(m, std)
 
 for i = 1 : size(m, 2) - 1
-    % Xavier initialization
-    var = 2/(m(i) + m(i + 1));
-    std = sqrt(var);
+    if nargin < 2
+        % Xavier initialization
+        var = 2/(m(i) + m(i + 1));
+        std = sqrt(var);
+    end
     
     W{i} = std*rand(m(i + 1), m(i));
     b{i} = zeros(m(i + 1), 1);
