93cf98d54abbfe39b3344521c42f065bf661e391
Tianxiao Zhao <tzh@kth.se>
2017-05-05 12:55:01 +0200

    add Xavier initialization

--- assign3/code/basic/init_para.m
+++ assign3/code/basic/init_para.m
@@ -1,6 +1,10 @@
-function [W, b] = init_para(m, std)
+function [W, b] = init_para(m, ~)
 
 for i = 1 : size(m, 2) - 1
+    % Xavier initialization
+    var = 2/(m(i) + m(i + 1));
+    std = sqrt(var);
+    
     W{i} = std*rand(m(i + 1), m(i));
     b{i} = zeros(m(i + 1), 1);
 end
